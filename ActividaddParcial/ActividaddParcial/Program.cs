926307e34855144db8843ae27b69745f924df151
MicaCaceres <mica.caceres@live.com>
2023-06-08 09:18:46 -0300

    Primer resolucion realizada a papel

--- ActividaddParcial/ActividaddParcial/Program.cs
+++ ActividaddParcial/ActividaddParcial/Program.cs
@@ -10,6 +10,39 @@ namespace ActividaddParcial
     {
         static void Main(string[] args)
         {
+            Calcular numC = new Calcular();
+            {
+
+            }
+
+             void Menu();
+            {
+                int opcion = 0; int valor = 0;
+                Console.WriteLine("1-Ingresar valor");
+                Console.WriteLine("2-Calcular Promedio");
+                Console.WriteLine("3-Salir");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+
+                do
+                {
+                    switch (opcion)
+                    {
+                        case 1:Console.WriteLine("Ingrese valor a agregar");
+                            valor = Convert.ToInt32(Console.ReadLine());
+                            numC.AgregarValor(valor);
+                            break;
+                        case 2:
+                            
+                            Console.WriteLine(numC.CalcularPromedio);
+                                break;
+                        default:
+                            Console.WriteLine("Se cerrara el programa");
+
+                    }
+
+                } while (opcion > 0 && opcion < 3);
+            }
         }
     }
 }
--- ActividaddParcial/ActividaddParcial/Promedio.cs
+++ ActividaddParcial/ActividaddParcial/Promedio.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ActividaddParcial
 {
-    internal class Promedio
+    internal class Calcular
     {
+        int valor = 0; int cont = 0;
+        double total = 0;
+
+        public int AgregarValor(int valor);{
+        cont++;
+            total+=valor;
+            return total;
+        }
+    public double CalcularPromedio();
+    {
+        if (cont==0 && total==0){
+        Console.WriteLine("no se puede calcular sin ingresos: el promedio es 0");
+        else return(total/cont*100)
+    }
+         
+    }
+
     }
 }
